4f615bbfafb98a7003954d15be5c1620661742cd
Tyler King <tyler.n.king@gmail.com>
2016-11-25 15:21:59 -0330

    Fix to RailsApp class

--- lib/shopify_app_whitelist/railite.rb
+++ lib/shopify_app_whitelist/railite.rb
@@ -3,7 +3,7 @@ module ShopifyAppWhitelist
   class Railtie < Rails::Railtie
     config.after_initialize do
       # Include our concern into the ApplicationController after initialization
-      Rails::app_class::ApplicationController.class_eval do
+      Rails.app_class::ApplicationController.class_eval do
         include ShopifyAppWhitelist::ProtectionConcern
       end
     end
